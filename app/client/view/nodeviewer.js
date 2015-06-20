14d68c9e6c852d165e4d140cf24a67bdcb6409dc
Tyler Smith <tylsmith@gmail.com>
2015-06-20 11:06:15 -0700

    body editing working

--- app/client/view/nodeviewer.js
+++ app/client/view/nodeviewer.js
@@ -1,5 +1,6 @@
 
-
+// At some point, this should maybe grab the local copy of the data instead
+// of the database copy
 Template.nodeviewer.selected = function () {
   var selected_id = Session.get("selected");
   return Nodes.findOne({_id: selected_id}) || Links.findOne({_id: selected_id});
@@ -12,9 +13,22 @@ Template.nodeviewer.registeredCommenter = function () {
 	return null;
 }
 
+Template.nodeviewer.helpers({
+  editingBody: function(){
+    return Session.get("editing-body");
+  }
+});
+
 Template.nodeviewer.events({
 	'click #delete-node': function () {
 		Meteor.call("deleteNode", Session.get("selected"));
 		Session.set("selected", undefined);
-	}
+	},
+  'click #edit-body': function () {
+    Session.set("editing-body", true);
+  },
+  'click #save-body': function() {
+    Nodes.update(Session.get("selected"), {$set:{body:$("#textarea-body").val()}});
+    Session.set("editing-body", false);
+  }
 })
